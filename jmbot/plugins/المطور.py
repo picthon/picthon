2655c4e15acafdfab6b8f03356a008018675e9f2
picthon <132414308+picthon@users.noreply.github.com>
2023-05-29 11:49:59 +0300

    Update المطور.py

--- jmbot/plugins/المطور.py
+++ jmbot/plugins/المطور.py
@@ -26,7 +26,7 @@ plugin_category = "utils"
     pattern="المطور$",
     command=("المطور", plugin_category),
     info={
-        "header": "لأظهار مطورين السورس",
+        "header": "لأظهار مطور السورس",
         "usage": [
             "{tr}المطور",
         ],
@@ -39,15 +39,14 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    CAT_IMG = " https://telegra.ph/file/7c8eda4cbe37cc34bf93e.jpg "
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين الجوكر\n"
+        cat_caption = f"مطور بيكثون \n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- المطور  : @lMl10l\n"
-        cat_caption += f"- المطور  : @rd0r0\n"
+        cat_caption += f"- المطور  : @a_t_9\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
